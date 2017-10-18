016270079cec3023e115ac47a7725eb3d73fc118
Egor_Che <Egor_Che@users.noreply.github.com>
2017-10-18 20:22:22 +0300

    massiv slov

--- src/main/java/MassivSlov.java
+++ src/main/java/MassivSlov.java
@@ -1,17 +1,30 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class MassivSlov {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
-        String[] words;
-        String userChoice = "";
         int dlinaMassiva;
         System.out.println("Задайте количество слов в массиве");
         dlinaMassiva = input.nextInt();
-        //System.out.print("Введено число = " + dlinaMassiva);
+        String[] massivSlov = new String[dlinaMassiva];
+        System.out.println("Введите слова, по одному с новой строки");
+        for (int i = 0; i < massivSlov.length; i++) {
+            massivSlov[i] = input.next();
+        }
+        System.out.print("Вы ввели " + dlinaMassiva + " слов(а): ");
+        for (int i = 0; i < massivSlov.length; i++) {
+            System.out.print(massivSlov[i] + " ");
+        }
+        int iMax = -1, iMaxLength = -1;
+        for (int i = 0; i < massivSlov.length; i++) {
+            if (!"".equals(massivSlov[i]) && massivSlov[i].length() > iMaxLength) {
+                iMax = i;
+                iMaxLength = massivSlov[i].length();
+            }
 
-
-
-        //ляляляля изменение
+        }
+        System.out.println("Самое длинное слово: " + massivSlov[iMax]);
     }
 }
+
