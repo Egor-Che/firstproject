499a4252a7382c42667ee95b9f1eb6b17df24cee
Egor_Che <Egor_Che@users.noreply.github.com>
2017-10-18 18:39:12 +0300

    Initial commit

---
+++ src/main/java/MassivSlov.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MassivSlov {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String[] words;
+        String userChoice = "";
+        int dlinaMassiva;
+        System.out.println("Задайте количество слов в массиве");
+        dlinaMassiva = input.nextInt();
+        //System.out.print("Введено число = " + dlinaMassiva);
+    }
+}
